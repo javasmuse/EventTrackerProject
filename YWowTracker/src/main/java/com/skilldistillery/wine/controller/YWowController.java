bdf158ede81aadd858ed4f1945e024acd9a52cf8
javasmuse <candacelewis@mac.com>
2019-09-28 22:56:45 -0600

    R U D next up Create

--- YWowTracker/src/main/java/com/skilldistillery/wine/controller/YWowController.java
+++ YWowTracker/src/main/java/com/skilldistillery/wine/controller/YWowController.java
@@ -2,12 +2,14 @@ package com.skilldistillery.wine.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,49 +21,68 @@ import com.skilldistillery.wine.services.YWowService;
 @RestController
 @RequestMapping("api")
 public class YWowController {
-	
+
 	@Autowired
-	private YWowService svc; 
-	
+	private YWowService svc;
+
 	@GetMapping("ping")
 	public String ping() {
-		return "pong"; 
+		return "pong";
 	}
-	
+
 	@GetMapping("wine")
 	public List<Wine> listAllWines() {
 		List<Wine> wines = svc.findAll();
-		return wines; 
+		return wines;
 	}
-	
+
 	@GetMapping("wine/{wid}")
 	public Wine getById(@PathVariable("wid") int id) {
 		return svc.findById(id);
 	}
-	
+
 	@DeleteMapping("wine/{wid}")
-    public void destroyWine(@PathVariable("wid") int postId, HttpServletResponse resp) {
-    }
-	
-	@PutMapping("wine/wid")
-    public Wine replacePost(@PathVariable("wid") int wid, @RequestBody Wine wine) {
-        Wine wine2 = svc.updateById(wid, wine);
-        if(wine != null) {
-        	wine2.setName(wine.getName());
-        	wine2.setCountry(wine.getCountry());
-        	wine2.setRegion(wine.getRegion());
-        	wine2.setGrape(wine.getGrape());
-        	wine2.setPrice(wine.getPrice());
-        	wine2.setLocationPurchased(wine.getLocationPurchased());
-        	wine2.setRating(wine.getRating());
-        	wine2.setNotes(wine.getNotes());
-        	wine2.setYear(wine.getYear());
-        	wine2.setImage(wine.getImage());
-        	wine2.setWinery(wine.getWinery());
-        }
-        return wine2;
-    }
-    
-	
-	
+	public void destroyWine(@PathVariable("wid") int postId, HttpServletResponse resp) {
+	}
+
+	@PutMapping("wine/{wid}")
+	public Wine replaceWine(@PathVariable("wid") int wid, @RequestBody Wine wine, HttpServletResponse resp,
+			HttpServletResponse req) {
+		try {
+			wine = svc.updateById(wid, wine);
+			if (wine != null) {
+				resp.setStatus(404);
+			} else {
+				resp.setStatus(201);
+			}
+		} catch (Exception e) {
+			resp.setStatus(400);
+			e.printStackTrace();
+		}
+		return wine;
+	}
+
+	@PostMapping("wine")
+	public Wine addWine(@RequestBody Wine wine) {
+		wine = svc.createNew(wine);
+		return wine;
+	}
 }
+
+//	@PostMapping("wine")
+//	public Wine addWine(@RequestBody Wine wine, HttpServletResponse resp, HttpServletRequest req) {
+//		try { 
+//			svc.createNew(wine); 
+//			resp.setStatus(201);
+//			StringBuffer url = req.getRequestURL();
+//			System.err.println(url);
+//			url.append("/");
+//			url.append(wine.getId());
+//			resp.setHeader("Location", url.toString());
+//		} catch (Exception e) {
+//			resp.setStatus(400);
+//			e.printStackTrace();
+//		}
+//		return wine;
+//		}
+//	}
